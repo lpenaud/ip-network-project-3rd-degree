1fe1baad91d2bb8d9633e6f9791a39153f350508
Crenn Romain <romaincrennt@gmail.com>
2019-03-14 23:25:38 +0100

    Add files via upload

--- src/achat.c
+++ src/achat.c
@@ -1,7 +1,128 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+#include "helpers.h"
+
+/* Socket du serveur*/
+static int sock;
+
+/* Handler de SIGINT (Ctrl+C)*/
+static void handler(int signum)
+{
+	/* On ferme la soket*/
+	close(sock);
+
+	/* On dit au revoir*/
+	fprintf(stderr, "Receiving the signal %d\n", signum);
+	printf("Bye !\n");
+
+	/* On quitte le programme */
+	exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+	int port;
+
+	struct sigaction sa;
+
+	socklen_t addr_server_len;
+	struct sockaddr_in addr_server;
+	char buf[BUF_SOCK];
+
+	struct hostent *hote;
+
+	unsigned int send;
+
+	int len;
+
+	/* informations sur les tickets envoyé à Concert */
+	char *tickets = malloc(sizeof(int));
+
+
+
+	/* Vérification nombre d'argument */
+	if (argc != 3) {
+		printf("Usage: %s <port> <serveur>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	/* Vérification si l'arguement est bien un entier entre 1 et 65535 */
+	scanf_port(argv[1], port);
+
+
+
+	/* Récupération de l'adresse IP du serveur (à partir du nom) */
+	if ((hote = gethostbyname(argv[2])) == NULL) {
+		handle_error();
+	}
+
+	/* Création de la socket serveur */
+	if ((sock = socket(AF_INET, SOCK_STREAM,0)) == -1) {
+		handle_error();
+	}
+
+	/* Préparation de l'adresse du serveur */
+	addr_server.sin_family = AF_INET;
+	addr_server.sin_port = htons(port);
+	bcopy(hote->h_addr, &addr_server.sin_addr, hote->h_length);
+
+
+	addr_server_len = sizeof(addr_server);
+	/* Demande de connexion au serveur */
+	if ((connect(sock, (struct sockaddr *) &addr_server, addr_server_len)) == -1) {
+		handle_error();
+	}
+
+	/* On s'occupe du signal SIGINT (Ctrl+C) */
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa, NULL) == -1) {
+		handle_error();
+	}
+
+
+
+	/* Le serveur a accepté la connexion */
+	printf("Connecté au site Concert\n");
+	fflush(stdout);
+
+
+	while(1){
+
+
+
+		/* lecture des informations encoyé par l'application concert */
+		if ((len = read(sock, buf, (sizeof(char)*BUF_SOCK))) == -1) {
+			printf_warning(strerror(errno));
+			goto loop_end;
+		}
+		printf("%s\n",buf);
+
+		/* informations entré par le consommateur */
+		scanf("%s", tickets);
+
+		if ((send = write(sock, tickets, sizeof(int))) !=  sizeof(int))
+		{
+			/* Si la fonction a été interrompu par signal, on ignore l'erreur*/
+			if (errno == EINTR) continue;
+			/* Sinon l'erreur est plus grave on stop le programme*/
+			handle_error();
+		}
+
+
+	}
+loop_end:
+	close(sock);
+
+	return 0;
+
 }
--- src/helpers.c
+++ src/helpers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>     // gethostname
-#include <strings.h>    // bcopy
-#include <arpa/inet.h>  // inet_ntoa
+#include <unistd.h>
+#include <strings.h>
+#include <arpa/inet.h>
 #include <ifaddrs.h>
 #include <netdb.h>
 #include <errno.h>
@@ -12,126 +12,126 @@
 // Affiche les informations sur les cartes réseaux de famille AF_INET(6)
 int display_any_address(const int family, const ushort port)
 {
-    struct ifaddrs *ifaddr, *ifa;
-    char addr[NI_MAXHOST];
-    int sa_family, s = 0;
+	struct ifaddrs *ifaddr, *ifa;
+	char addr[NI_MAXHOST];
+	int sa_family, s = 0;
 
-    if (family != AF_INET && family != AF_INET6) {
-        printf("Family accepted: AF_INET or AF_INET6\n");
-        return -1;
-    }
+	if (family != AF_INET && family != AF_INET6) {
+		printf("Family accepted: AF_INET or AF_INET6\n");
+		return -1;
+	}
 
-    // Obtention de la liste des cartes réseaux
-    if (getifaddrs(&ifaddr) == -1) {
-        perror("getifaddrs");
-        return errno;
-    }
+	// Obtention de la liste des cartes réseaux
+	if (getifaddrs(&ifaddr) == -1) {
+		perror("getifaddrs");
+		return errno;
+	}
 
-    printf("Server listening :\n");
-    // On parcourt la liste des cartes réseaux
-    for (ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
-        if (ifa->ifa_addr == NULL) continue;
+	printf("Server listening :\n");
+	// On parcourt la liste des cartes réseaux
+	for (ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
+		if (ifa->ifa_addr == NULL) continue;
 
-        // Si la carte réseau est de la bonne famille
-        if ((sa_family = ifa->ifa_addr->sa_family) == family) {
-            // Obtention des informations de la carte
-            s = getnameinfo(ifa->ifa_addr,
-                sa_family == AF_INET
-                    ? sizeof(struct sockaddr_in)
-                    : sizeof(struct sockaddr_in6),
-                addr,
-                NI_MAXHOST,
-                NULL,
-                0,
-                NI_NUMERICHOST
-            );
-            // Si l'obtention des informations a échoué
-            if (s != 0) {
-                printf("getnameinfo() failed: %s\n", gai_strerror(s));
-                goto err;
-            }
-            /* Sinon on affiche les informations sur ce format :
-                - On <nom>:     <adresse ip>:<port>
-            */
-            printf("\t- On %s:\t%s%s:%s%hu%s\n",
-                ifa->ifa_name,
-                ANSI_CYAN,
-                addr,
-                ANSI_BOLD,
-                port,
-                ANSI_RESET
-            );
-        }
-    }
+		// Si la carte réseau est de la bonne famille
+		if ((sa_family = ifa->ifa_addr->sa_family) == family) {
+			// Obtention des informations de la carte
+			s = getnameinfo(ifa->ifa_addr,
+					sa_family == AF_INET
+					? sizeof(struct sockaddr_in)
+					: sizeof(struct sockaddr_in6),
+					addr,
+					NI_MAXHOST,
+					NULL,
+					0,
+					NI_NUMERICHOST
+				       );
+			// Si l'obtention des informations a échoué
+			if (s != 0) {
+				printf("getnameinfo() failed: %s\n", gai_strerror(s));
+				goto err;
+			}
+			/* Sinon on affiche les informations sur ce format :
+			   - On <nom>:     <adresse ip>:<port>
+			 */
+			printf("\t- On %s:\t%s%s:%s%hu%s\n",
+					ifa->ifa_name,
+					ANSI_CYAN,
+					addr,
+					ANSI_BOLD,
+					port,
+					ANSI_RESET
+			      );
+		}
+	}
 
 err:
-    // On libère la mémoire des cartes réseaux
-    freeifaddrs(ifaddr);
-    return s;
+	// On libère la mémoire des cartes réseaux
+	freeifaddrs(ifaddr);
+	return s;
 }
 
 // Permet d'éviter des bogues de buffer si on est dans une boucle
 char mgetchar(char fin) {
-    char c = fin, tmp;
-    while ((tmp = getchar()) != fin) {
-        c = tmp;
-    }
-    return c;
+	char c = fin, tmp;
+	while ((tmp = getchar()) != fin) {
+		c = tmp;
+	}
+	return c;
 }
 
 // Permet d'envoyer un buffer à plusieurs hôtes
 int boucle_send(int sock, void *buf, int lg, struct sockaddr_in *addrs, size_t len)
 {
-    int etat;
-    size_t i;
+	int etat;
+	size_t i;
 
-    for (i = 0; i < len; i++) {
-        etat = sendto(
-            sock,
-            buf,
-            lg,
-            0,
-            (struct sockaddr *) addrs + i,
-            sizeof(addrs[i])
-        );
-        if (etat != lg) return -1;
-    }
-    return 0;
+	for (i = 0; i < len; i++) {
+		etat = sendto(
+				sock,
+				buf,
+				lg,
+				0,
+				(struct sockaddr *) addrs + i,
+				sizeof(addrs[i])
+			     );
+		if (etat != lg) return -1;
+	}
+	return 0;
 }
 
 /*
-    Créer une nouvelle socket
-    l'attache sur un port donné et sur tout les cartes réseaux disponibles
-    Fait écouter la socket pour un nombre de connexions donné
+   Créer une nouvelle socket
+   l'attache sur un port donné et sur tout les cartes réseaux disponibles
+   Fait écouter la socket pour un nombre de connexions donné
 
-    Retourne le descripteur de fichier de la socket
-*/
+   Retourne le descripteur de fichier de la socket
+ */
 int listen_new_socket(int domain, int type, int protocol, ushort port, int nb_conn)
 {
-    int sock;
-    struct sockaddr_in addrLocale;
+	int sock;
+	struct sockaddr_in addrLocale;
 
-    // Création de la socket
-    if ((sock = socket(domain, type, protocol)) == -1)
-        return -1;
+	// Création de la socket
+	if ((sock = socket(domain, type, protocol)) == -1)
+		return -1;
 
-    // Préparation de l'adresse locale
-    addrLocale.sin_family = domain;
-    addrLocale.sin_port = htons(port);
-    addrLocale.sin_addr.s_addr = htonl(INADDR_ANY);
-    // Attache la socket sur `port` port
-    if (bind(sock, (struct sockaddr *) &addrLocale, sizeof(addrLocale)) == -1) {
-        close(sock);
-        return -2;
-    }
-    // La socket écoute pour `nb_conn` connexion
-    if (listen(sock, nb_conn) != 0) {
-        close(sock);
-        return -3;
-    }
-    // On affiche les informations des cartes réseau compatible
-    display_any_address(addrLocale.sin_family, port);
+	// Préparation de l'adresse locale
+	addrLocale.sin_family = domain;
+	addrLocale.sin_port = htons(port);
+	addrLocale.sin_addr.s_addr = htonl(INADDR_ANY);
+	// Attache la socket sur `port` port
+	if (bind(sock, (struct sockaddr *) &addrLocale, sizeof(addrLocale)) == -1) {
+		close(sock);
+		return -2;
+	}
+	// La socket écoute pour `nb_conn` connexion
+	if (listen(sock, nb_conn) != 0) {
+		close(sock);
+		return -3;
+	}
+	// On affiche les informations des cartes réseau compatible
+	display_any_address(addrLocale.sin_family, port);
 
-    // On retourne le descripteur de fichier
-    return sock;
+	// On retourne le descripteur de fichier
+	return sock;
 }
